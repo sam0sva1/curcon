81190509c2adc1ef1167f4c806fbeb43d2b44e43
sam0sva1 <konstantus@live.ru>
2018-02-13 23:06:35 +0300

    CC adds a list and ListItems

---
+++ app/components/List/index.js
@@ -0,0 +1,7 @@
+import styles from './styles';
+import Separator from './Separator';
+import Icon from './Icon';
+import ListItem from './ListItem';
+
+
+export { styles, ListItem, Icon, Separator };
