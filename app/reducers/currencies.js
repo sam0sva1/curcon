060285261b731d725222b885f8a0c32a7502a804
sam0sva1 <konstantus@live.ru>
2018-02-15 15:06:12 +0300

    CC connects the CurrencyList to Redux store

--- app/reducers/currencies.js
+++ app/reducers/currencies.js
@@ -1,4 +1,4 @@
-import { SWAP_CURRENCY, CHANGE_CURRENCY_AMOUNT } from '../actions/currencies';
+import { SWAP_CURRENCY, CHANGE_CURRENCY_AMOUNT, CHANGE_BASE_CURRENCY, CHANGE_QUOTE_CURRENCY } from '../actions/currencies';
 
 
 // const initialState = {
@@ -56,6 +56,23 @@ const initialState = {
   },
 };
 
+const setConvertions = (state, action) => {
+  let conversion = {
+    isFetching: true,
+    data: '',
+    rates: {},
+  };
+
+  if (state.conversions[action.currency]) {
+    conversion = state.conversions[action.currency];
+  }
+
+  return {
+    ...state,
+    [action.currency]: conversion,
+  };
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case CHANGE_CURRENCY_AMOUNT:
@@ -68,6 +85,18 @@ const reducer = (state = initialState, action) => {
         ...state,
         baseCurrency: state.quoteCurrency,
         quoteCurrency: state.baseCurrency,
+      };
+    case CHANGE_BASE_CURRENCY:
+      return {
+        ...state,
+        baseCurrency: action.currency,
+        conversions: setConvertions(state, action),
+      };
+    case CHANGE_QUOTE_CURRENCY:
+      return {
+        ...state,
+        quoteCurrency: action.currency,
+        conversions: setConvertions(state, action),
       };
     default:
       return state;
--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { FlatList, View, StatusBar } from 'react-native';
 
 import { ListItem, Separator } from '../components/List';
 import currenciesArray from '../data/currencies';
+import { changeBaseCurrency, changeQuoteCurrency } from '../actions/currencies';
 
 const TEMP_CURRENT_CURRENCY = 'CAD';
 
+@connect()
 class CurrencyList extends Component {
   static propTypes = {
     navigation: PropTypes.shape({
       goBack: PropTypes.func,
     }),
+    dispatch: PropTypes.func,
   }
 
-  handlePress = () => {
+  handlePress = (currency) => {
+    const { type } = this.props.navigation.state.params;
+    if (type === 'base') {
+      this.props.dispatch(changeBaseCurrency(currency));
+    } else if (type === 'quote') {
+      this.props.dispatch(changeQuoteCurrency(currency));
+    }
     this.props.navigation.goBack(null);
   }
 
@@ -28,7 +38,7 @@ class CurrencyList extends Component {
             <ListItem
               text={item}
               selected={item === TEMP_CURRENT_CURRENCY}
-              onPress={this.handlePress}
+              onPress={() => this.handlePress(item)}
             />
           )}
           keyExtractor={item => item}
